d16d303f28fd229734b0a89c623c308472b6e6ef
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2021-11-29 23:38:35 +0530

    added final

--- app.py
+++ app.py
@@ -17,8 +17,7 @@ app.config.from_object('config.Config')
 # database connection
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///registration.db'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'postgresql://zycvbnvwvumfpo:ec9a7853e5f5b94eaf0d413d5a38288d16ba770cd1532f98818daa091219e73b@ec2-54-195-76-73.eu-west-1.compute.amazonaws.com:5432/dc77499m8jiavo'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///registration.db'
 db.init_app(app)
 
 
@@ -32,11 +31,11 @@ app = urls.add_url(app)
 if __name__ == "__main__":
     #  run this db.create first timels
 
-    # try:
-    #     with app.app_context():
-    #         db.create_all()
-    # except Exception as e:
-    #     print(e)
+    try:
+        with app.app_context():
+            db.create_all()
+    except Exception as e:
+        print(e)
     app.run(debug=True)
 
 
